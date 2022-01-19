d4fb4ed503ad9d5fe96334b2792708807a632c0b
zubair1shareef <66367807+zubair1shareef@users.noreply.github.com>
2022-01-19 23:10:21 +0530

    initial push

---
+++ formput.js
@@ -0,0 +1,15 @@
+const form=document.querySelector('#my-form');
+const name=document.querySelector('#name');
+const email=document.querySelector('#email');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+  if(name.value===""|| email.value===""){
+      alert("fill all the fields")
+  }
+  else{
+      console.log(name.value+","+email.value)
+  }
+})
+
+
